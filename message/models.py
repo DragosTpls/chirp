d9c14d140c62939c25c957e46139a2655eddbdbd
DragosTpls <dragos.tepelus@gmail.com>
2017-07-21 11:36:08 +0300

    v.1.0

--- message/models.py
+++ message/models.py
@@ -1,23 +1,27 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class TimestampModel(models.Model):
     class Meta:
-        abstract=True
+        abstract = True
     created = models.DateTimeField(auto_now_add=True)
 
 
 class Message(TimestampModel):
-    user=models.ForeignKey(User)
-    status=models.TextField(null=False, max_length=140, blank=False)
+    user = models.ForeignKey(User)
+    status = models.TextField(null=False, max_length=140, blank=False)
 
+    def __str__(self):
+        return self.status
 
 
 class Like(TimestampModel):
     user = models.ForeignKey(User)
-    message=models.ForeignKey(Message)
-    like=models.BooleanField()
+    message =models.ForeignKey(Message)
+    like = models.BooleanField()
+
 
 class Follow(TimestampModel):
-    followed_user=models.ForeignKey(User, related_name='followed_by')
+    followed_user = models.ForeignKey(User, related_name='followed_by')
     following_user = models.ForeignKey(User, related_name='following')
